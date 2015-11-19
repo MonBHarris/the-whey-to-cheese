fe9dcdc4eb498dd80e3d09a59302496fa7802064
Monica <mon.b.harris@gmail.com>
2015-11-18 22:21:48 -0500

    add filtering and logic

--- js/wheytocheese.js
+++ js/wheytocheese.js
@@ -1,46 +1,155 @@
 "use strict";
 
-var AllCheeses = new Array;
+var allCheeses = [
+  {
+    id: "1",
+    name: 'Bayley Hazen Blue',
+    flavor: 'funky, nutty',
+    texture: 'smooth',
+    milkType: 'cow',
+    originCountry: 'USA',
+    goodFor: 'adventerous'
+  },
+  {
+    id: "2",
+    name: 'Beemster Gouda',
+    flavor: 'nutty, funky',
+    texture: 'firm',
+    milkType: 'cow',
+    originCountry: 'Holland',
+    goodFor: 'everyone'
+  }
+];
 
-//Cheese Constructor 
-
-function Cheese(name, flavor, texture, milkType, originCountry, goodFor){
-  this.name = name;
-  this.flavor = flavor;
-  this.texture = texture;
-  this.milkType = milkType;
-  this.originCountry = originCountry;
-  this.goodFor = goodFor;
-  AllCheeses.push(this);
-  this.image = '<img src= images/'+this.name+".jpg></img>";
+function setupCheese(cheese) {
+  var cheeseDiv = "<div id='cheese"+cheese.id+"'></div>";
+  $(cheeseDiv).appendTo('#cheeseDisplay');
+  appendCheeseTitle(cheese, "name");
+  appendCheeseParagraph(cheese, "flavor");
+  appendCheeseParagraph(cheese, "texture");
+  appendCheeseParagraph(cheese, "milkType")
+  appendCheeseParagraph(cheese, "originCountry")
+  appendCheeseParagraph(cheese, "goodFor");
 }
 
-//Populate Name Field
+function appendCheeseTitle(cheese, attribute) {
+  var $cheeseDiv = $("#cheese"+cheese.id);
+  $cheeseDiv.append("<h3>"+cheese[attribute]+"</h3>");
+}
 
-Cheese.prototype.El = function() {
-    return "<ul id="+this.id+'>'+this.name+
-             '<li>'+this.flavor+"</li>"+
-           "</ul>";
+function appendCheeseParagraph(cheese, attribute) {
+  var $cheeseDiv = $("#cheese"+cheese.id);
+  $cheeseDiv.addClass(cheese[attribute].replace(',', ''))
+  $cheeseDiv.append("<p>"+cheese[attribute]+"</p>");
+}
+
+function setupCheeses() { 
+  $.each(allCheeses, function(i, cheese){
+    setupCheese(cheese);
+  });
+}
+
+function setupCheeseFilter(filterCollection, filterId) {
+  $.each(filterCollection, function (i, filter){
+    $('<input type="checkbox" value="'+filter+'" ><span>'+filter+'</span>').appendTo('#'+filterId);  
+  });
+}
+
+function monitorCheeseFilters() {
+  $("#filters input").change(function() {
+    applyCheeseFilters();
+  })
+}
+
+function applyCheeseFilters() {
+  var $allCheeseDivs = $('#cheeseDisplay div')
+  $allCheeseDivs.show();
+
+  var allCheckedFilters = $('#filters input:checked');
+
+  $.each(allCheckedFilters, function(i, filter) {
+    var $filterSelector = $(this);
+    var klassName = $filterSelector.val();
+
+    $.each($allCheeseDivs, function(i, cheeseDiv){
+      var showDiv = $(cheeseDiv).hasClass(klassName) && $(cheeseDiv).is(":visible");
+      if(!showDiv) {
+        $(cheeseDiv).hide();
+      }
+    });
+  });
+}
+
+function setupCheeseFlavorFilter() {
+  var cheeseFlavors = gatherCheeseFlavors();
+  setupCheeseFilter(cheeseFlavors, "flavor")
+};  
+
+function setupCheeseTextureFilter() {
+  var cheeseTextures = gatherCheeseTextures();
+  setupCheeseFilter(cheeseTextures, "texture")
+};  
+
+function setupCheeseMilkTypeFilter() {
+  var cheeseMilkTypes = gatherCheeseMilkTypes();
+  setupCheeseFilter(cheeseMilkTypes, "milkType")
 };
 
-Cheese.prototype.create = function() {
-    $(this.El()).addClass(this.flavor+' '+this.texture).appendTo('#CheeseDisplay');
+function setupCheeseOriginCountry() {
+  var cheeseOriginCountries = gatherCheeseOriginCountries();
+  setupCheeseFilter(cheeseOriginCountries, "originCountry")
 };
 
+function setupCheeseGoodFor() {
+  var cheeseGoodFor = gatherCheeseGoodFor();
+  setupCheeseFilter(cheeseGoodFor, "goodFor")
+};
 
-//Cheeses
+function gatherCheeseAttribute(attributeName) {
+  var attributeValues = [];
 
-var BayleyHazenBlue = new Cheese("Bayley Hazen Blue", "funky", "smooth", "cow", "USA", "adventerous eaters"); 
-var BeemsterGouda = new Cheese("Beemster Gouda", "nutty and sweet", "firm", "cow", "Holland", "everyone"); 
+  $.each(allCheeses, function (i, cheese) {
+    attributeValues.push(cheese[attributeName].split(", "));
+  });
 
-//Create Cheese Method
+  var attributeValues = [].concat.apply([], attributeValues);
+  return $.unique(attributeValues);
+}
 
-//Creating Cheeses
+function gatherCheeseFlavors() {
+  return gatherCheeseAttribute("flavor");
+}
 
-BayleyHazenBlue.create();
-BeemsterGouda.create();
+function gatherCheeseTextures() {
+  return gatherCheeseAttribute("texture");
+}
 
- $(document).ready(function(){
+function gatherCheeseMilkTypes() {
+  return gatherCheeseAttribute("milkType");
+}
 
+function gatherCheeseOriginCountries() {
+  return gatherCheeseAttribute("originCountry");
+}
 
- });
+function gatherCheeseGoodFor() {
+  return gatherCheeseAttribute("goodFor");
+}
+
+function setupFilters() {
+  setupCheeseFlavorFilter();
+  setupCheeseTextureFilter();
+  setupCheeseMilkTypeFilter();
+  setupCheeseOriginCountry();
+  setupCheeseGoodFor();
+}
+
+function init() {
+  setupFilters();
+  setupCheeses();
+  monitorCheeseFilters();
+}
+
+$(document).ready(function(){
+  init();
+});
